938cd01c10c0d21e996e92f9162a9e052ca26466
林凤强(10019929) <linfq@glodon.com>
2019-12-03 20:30:09 +0800

    first commit

---
+++ components/trailerStars.vue
@@ -0,0 +1,71 @@
+<template name="trailerStars">
+	<view class="movie-score-wapper">
+		<!-- 
+		<image src="../../static/image/icon/star-yellow.png" class="star-ico"></image>
+		<image src="../../static/image/icon/star-yellow.png" class="star-ico"></image>
+		<image src="../../static/image/icon/star-yellow.png" class="star-ico"></image>
+		<image src="../../static/image/icon/star-yellow.png" class="star-ico"></image>
+		<image src="../../static/image/icon/star-gray.png" class="star-ico"></image>
+		<view class="movie-score">
+			9.1
+		</view>
+		 -->
+		<image v-for="yellow in yellowScore"
+		 src="../../static/image/icon/star-yellow.png" class="star-ico"></image>
+		<image v-for="gray in grayScore"
+		 src="../../static/image/icon/star-gray.png" class="star-ico"></image>
+		 <view class="movie-score" v-if="showNum == 1">
+		 	{{innerScore}}
+		 </view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "trailerStars",
+		data() {
+			return {
+				yellowScore: 0,
+				grayScore: 5
+			};
+		},
+		props: {
+			// 定义外部传入的分数
+			innerScore: 0,
+			// 是否显示具体的分数 1：显示，0：不显示
+			showNum: 0
+		},
+		// 生命周期，组件创建完成后调用
+		created() {
+			var tempscore = 0;
+			if (this.innerScore != null && this.innerScore != undefined && this.innerScore != '') {
+				tempscore = this.innerScore;
+			}
+			
+			var yellowScore = parseInt(tempscore / 2);
+			var grayScore = 5 - yellowScore;
+			
+			this.yellowScore = yellowScore;
+			this.grayScore = grayScore;
+		}
+	}
+</script>
+
+<style>
+.movie-score-wapper {
+	display: flex;
+	flex-direction: row;
+	margin-top: 6upx;
+}
+
+.star-ico {
+	width: 20upx;
+	height: 20upx;
+}
+
+.movie-score {
+	font-size: 12px;
+	color: gray;
+	margin-left: 8upx;
+}
+</style>
---
+++ unpackage/dist/dev/.tmp/app-plus/pages/community/community.js
@@ -0,0 +1,26 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/community/community"],{
+
+/***/ 39:
+/*!***********************************************************************************************************************!*\
+  !*** D:/Program Files/HBuilderX/HBuilderProjects/next-superhero-dev/main.js?{"page":"pages%2Fcommunity%2Fcommunity"} ***!
+  \***********************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 4);__webpack_require__(/*! @dcloudio/uni-stat */ 5);\n\nvar _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 3));\nvar _community = _interopRequireDefault(__webpack_require__(/*! ./pages/community/community.vue */ 40));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}\ncreatePage(_community.default);\n/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-app-plus/dist/index.js */ 1)[\"createPage\"]))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbbnVsbF0sIm5hbWVzIjpbImNyZWF0ZVBhZ2UiLCJQYWdlIl0sIm1hcHBpbmdzIjoia0RBQUEsd0NBQW1COztBQUVuQjtBQUNBLHdHO0FBQ0FBLFVBQVUsQ0FBQ0Msa0JBQUQsQ0FBVixDIiwiZmlsZSI6IjM5LmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0ICd1bmktcGFnZXMnO2ltcG9ydCAnQGRjbG91ZGlvL3VuaS1zdGF0JztcblxuaW1wb3J0IFZ1ZSBmcm9tICd2dWUnICAgICAgICAgICAgXG5pbXBvcnQgUGFnZSBmcm9tICcuL3BhZ2VzL2NvbW11bml0eS9jb21tdW5pdHkudnVlJ1xuY3JlYXRlUGFnZShQYWdlKSJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///39\n");
+
+/***/ }),
+
+/***/ 40:
+/*!****************************************************************************************************!*\
+  !*** D:/Program Files/HBuilderX/HBuilderProjects/next-superhero-dev/pages/community/community.vue ***!
+  \****************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\\nError: ENOENT: no such file or directory, open 'D:\\\\Program Files\\\\HBuilderX\\\\HBuilderProjects\\\\next-superhero-dev\\\\pages\\\\community\\\\community.vue'\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiI0MC5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///40\n");
+
+/***/ })
+
+},[[39,"common/runtime","common/vendor"]]]);
